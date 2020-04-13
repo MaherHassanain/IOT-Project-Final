41b54d09dd47f12576afebfe5aeb9378a78879cf
MaherHassanain <maher_hassanain@hotmail.com>
2020-04-13 16:36:15 -0400

    final adjustments

--- Broker.py
+++ Broker.py
@@ -45,5 +45,5 @@ if __name__ == '__main__':
     formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
     logging.basicConfig(level=logging.INFO, format=formatter)
     asyncio.get_event_loop().run_until_complete(startBroker())
-    asyncio.get_event_loop().run_until_complete(brokerGetMessage())
+    # asyncio.get_event_loop().run_until_complete(brokerGetMessage())
     asyncio.get_event_loop().run_forever()
